0bb0442ec78343416e8974e0bd962610d436328f
Misha Poleshchenkov <mikhail.poleshchenkov@mail.ru>
2022-02-12 21:20:07 +0300

    update

--- src/components/UI/index.js
+++ src/components/UI/index.js
@@ -1,12 +1,10 @@
 import MyButton from "@/components/UI/MyButton"
 import MyInput from "@/components/UI/MyInput"
 import Divider from "@/components/UI/Divider"
-import MySelect from '@/components/UI/MySelect'
 
 
 export default [
     MyButton,
     MyInput,
     Divider,
-    MySelect
 ]
